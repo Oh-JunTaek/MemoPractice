fb508f7177f4d92df45c2d007e02fed10384ed3c
eunma <aa39043533@gmail.com>
2023-12-15 16:10:58 +0900

    메모장 추가삭제 기능

--- app/src/main/java/com/example/memopractice/MainActivity.kt
+++ app/src/main/java/com/example/memopractice/MainActivity.kt
@@ -1,22 +1,25 @@
 package com.example.memopractice
 
 fun main() {
-    val memos = mutableMapOf<String, String>()
     val memoManager = MemoManager()
+    println("안녕하세요 메모장입니다.")
 
     while (true) {
-        println("안녕하세요 메모장입니다.")
         println("새로운 메모를 작성하시려면 1, 저장된 메모를 보려면 2, 메모장을 종료하려면 0을 입력하세요.")
-        when (readLine()) {
+        when (readLine() ?: "") {
             "1" -> {
-                println("메모의 제목을 입력해 주세요.")
+                println("메모의 제목을 입력해 주세요. 취소하려면 /main을 입력해 주세요")
                 val title = readLine() ?: ""
+                if (title == "/main") {
+                    println("메모 작성을 취소합니다.")
+                    continue
+                }
                 println("메모 내용을 입력해 주세요. 메모 작성을 취소하려면 /main 을 입력해주세요.")
                 val content = readLine() ?: ""
                 if (content == "/main") {
                     println("메모 작성을 취소합니다.")
                 } else {
-                    memoManager.add("$title: $content")
+                    memoManager.add(Memo(title, content))
                     println("메모가 저장되었습니다.")
                 }
             }
@@ -26,11 +29,45 @@ fun main() {
                     println("저장된 메모가 없습니다.")
                 } else {
                     memos.forEachIndexed { index, memo ->
-                        println("${index + 1}. $memo")
+                        println("${index + 1}. ${memo.title}")
                     }
-                    println("보고 싶은 메모의 번호를 입력하세요.")
-                    val index = readLine()?.toIntOrNull() ?: 0
-                    println(memoManager.get(index - 1) ?: "잘못된 번호입니다.")
+                    println("보고 싶은 메모의 번호를 입력하세요. 처음 화면으로 돌아가려면 0을 입력하세요.")
+                    val index = readLine()?.toIntOrNull() ?: -1
+                    if (index == 0) {
+                        continue
+                    }
+                    val selectedMemo = memoManager.get(index - 1)
+                    if (selectedMemo != null) {
+                        println("제목: ${selectedMemo.title}")
+                        println("내용: ${selectedMemo.content}")
+                        println("메모를 수정하려면 1, 메모를 삭제하려면 2, 처음 화면으로 돌아가려면 0을 입력하세요.")
+                        when (readLine() ?: "") {
+                            "1" -> {
+                                println("새 제목을 입력해 주세요. 취소하려면 /main을 입력해 주세요")
+                                val newTitle = readLine() ?: ""
+                                if (newTitle == "/main") {
+                                    println("메모 수정을 취소합니다.")
+                                    continue
+                                }
+                                println("새 내용을 입력해 주세요. 취소하려면 /main을 입력해 주세요")
+                                val newContent = readLine() ?: ""
+                                if (newContent == "/main") {
+                                    println("메모 수정을 취소합니다.")
+                                } else {
+                                    memoManager.update(index - 1, Memo(newTitle, newContent))
+                                    println("메모가 수정되었습니다.")
+                                }
+                            }
+                            "2" -> {
+                                memoManager.remove(index - 1)
+                                println("메모가 삭제되었습니다.")
+                            }
+                            "0" -> continue
+                            else -> println("[ERROR] 입력한 값을 다시 확인해 주세요.")
+                        }
+                    } else {
+                        println("[ERROR] 입력한 값을 다시 확인해 주세요.")
+                    }
                 }
             }
             "0" -> {
@@ -38,7 +75,7 @@ fun main() {
                 return
             }
             else -> {
-                println("잘못된 입력입니다.")
+                println("[ERROR] 입력한 값을 다시 확인해 주세요.")
             }
         }
     }
--- app/src/main/java/com/example/memopractice/MemoManager.kt
+++ app/src/main/java/com/example/memopractice/MemoManager.kt
@@ -1,21 +1,39 @@
 package com.example.memopractice
 
 class MemoManager {
-    private val memos = mutableListOf<String>()
+    private val memos = mutableListOf<Memo>()
 
-    fun add(memo: String) {
+    fun add(memo: Memo) {
         memos.add(memo)
     }
 
-    fun list(): List<String> {
+    fun list(): List<Memo> {
         return memos
     }
 
-    fun get(index: Int): String? {
+    fun get(index: Int): Memo? {
         return if (index in memos.indices) {
             memos[index]
         } else {
             null
+        }
+    }
+
+    fun update(index: Int, memo: Memo): Boolean {
+        return if (index in memos.indices) {
+            memos[index] = memo
+            true
+        } else {
+            false
+        }
+    }
+
+    fun remove(index: Int): Boolean {
+        return if (index in memos.indices) {
+            memos.removeAt(index)
+            true
+        } else {
+            false
         }
     }
 }
